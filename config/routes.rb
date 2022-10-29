4b266870adbb099592c98c4345f9d40c6825ffc9
Marimo-R <113738982+Marimo-R@users.noreply.github.com>
2022-10-29 02:32:41 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,29 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  
+  scope module: :public do
+    root to: "homes#top"
+    get 'about' => 'homes#about'
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/information/edit' => 'customers#edit'
+    patch 'customers/information' => 'customers#update'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdrawal' => 'customers#withdrawal'
+    resources :items, only:[:show, :index]
+    resources :cart_items, only:[:index, :update, :destroy, :create]
+    delete 'cart_items' => 'cart_items#all_destroy'
+    resources :orders, only:[:new, :create, :index, :show]
+    post 'orders/check' => 'orders#check'
+    get 'orders/:id/complete' => 'orders#complete'
+    resources :delivery_addresses, only:[:index, :edit, :create, :update, :destroy]
+  end
+  
+  namespace :admin do
+    root to: "homes#top"
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
