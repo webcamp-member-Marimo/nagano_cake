5f7037a76246a8260102644681408bdaa58ab7a3
Marimo-R <113738982+Marimo-R@users.noreply.github.com>
2022-11-01 09:32:41 +0000

    [Finish]create add items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,28 @@
 class Admin::ItemsController < ApplicationController
   def index
   end
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active)
   end
 end
