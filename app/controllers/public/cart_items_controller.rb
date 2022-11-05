210f238c7b546e6907e1a46a11fa4110095fc17a
Marimo-R <113738982+Marimo-R@users.noreply.github.com>
2022-11-05 05:03:39 +0000

    [Finish]add item-show

---
+++ app/controllers/public/cart_items_controller.rb
@@ -0,0 +1,15 @@
+class Public::CartItemsController < ApplicationController
+  def index
+  end
+  
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
+end
