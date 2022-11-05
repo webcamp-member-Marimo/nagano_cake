f5a3c04ddf60e11bf368e42c9c9c3deef0961351
Marimo-R <113738982+Marimo-R@users.noreply.github.com>
2022-11-05 01:38:55 +0000

    [Finish]withdrawal function

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,6 +13,16 @@ class Public::CustomersController < ApplicationController
     redirect_to customers_my_page_path
   end
   
+  def unsubscribe
+  end
+  
+  def withdrawal
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+  
   private
   def params_customer
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,16 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  before_action :customer_state, only: [:create]
+  
+  protected
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted
+    else
+      redirect_to new_customer_registration_path
+    end
+  end
 end
