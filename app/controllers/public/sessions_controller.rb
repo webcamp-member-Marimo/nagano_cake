49b9a8457b266f3517622e85e2ad70c43ad89f7c
Marimo-R <113738982+Marimo-R@users.noreply.github.com>
2022-11-15 00:18:56 +0000

    modify

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(resource)
+    root_path
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,9 +27,9 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
+
   before_action :customer_state, only: [:create]
-  
+
   protected
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
